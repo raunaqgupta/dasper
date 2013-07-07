758160f0f5135773ddbe2fac4038e1c9b16bdd26
Raunaq Gupta <raunaq.rg@gmail.com>
2013-07-07 01:23:44 +0100

    More updates. It is looking good.

--- js/data.js
+++ js/data.js
@@ -113,12 +113,13 @@ DASHBOARD.LastFM = function(username, num_weeks){
 					date_array.push([parseInt(x['from']), parseInt(x['to'])]);
 				}
 
+				console.log(date_array);
 				//populate week_data
 				var j=0;
 
 				_.each(date_array, function(date){
 					lastfm.user.getWeeklyTrackChart({user:username, from:date[0], to:date[1]},{success: function(data){
-
+						console.log(data);
 						var playcount = 0;
 						j++;
 						for(var i=0; i<data.weeklytrackchart.track.length;i++){
@@ -338,9 +339,7 @@ DASHBOARD.FourSquare = function(access_token, user_id){
 			canvas_parent.append(canvas);
 		}
 
-		var myRadar = new Chart(canvas.get(0).getContext("2d")).Radar(venue_chart_data,{
-			pointLabelFontSize : 12
-		});
+		var myRadar = new Chart(canvas.get(0).getContext("2d")).Pie(venue_chart_data);
 	}
 
 	return {
@@ -423,23 +422,44 @@ DASHBOARD.FourSquare = function(access_token, user_id){
 					});
 				});
 
-				var labels = [], values = [];
-				_.sortBy
-				_.each(venue_categories, function(num, key){
-					if(num>10){
-						labels.push(key);
-						values.push(num);
-					}
+
+
+				var tuples = _.map(venue_categories, function(value, key){ return [key, value]; });
+
+				var sorted_categories = _.sortBy(tuples, function(tuple){
+					return tuple[1];
 				});
 
-				venue_chart_data["labels"] = labels;
-				venue_chart_data["datasets"] = [{
-					fillColor : "rgba(220,220,220,0.5)",
-					strokeColor : "rgba(220,220,220,1)",
-					pointColor : "rgba(220,220,220,1)",
-					pointStrokeColor : "#fff",
-					data : values
-				}];
+				var top3_categories = _.first(sorted_categories.reverse(), 3);
+				venue_chart_data = [
+					{
+						value: top3_categories[0][1],
+						color: "#00CC00"
+					},
+					{
+						value: top3_categories[1][1],
+						color: "#FF7400"
+					},
+					{
+						value: top3_categories[2][1],
+						color: "#CD0074"
+					}
+				];
+
+				var legend_ul = $("#venue_legend");
+				_.each(top3_categories, function(element, index, list){
+					var color = venue_chart_data[index].color;
+					legend_ul.append(
+						$('<li>').append(
+							$('<div>')
+								.css("background-color",color)
+								.css("width","20px")
+								.css("height","20px")
+								.css("float","left")
+								.css("margin-right","5px"),
+							element[0])
+						);
+				});
 
 				//draw the chart
 				drawVenueCanvas();
@@ -481,6 +501,7 @@ DASHBOARD.facebook = function(access_token){
 	var until_timestamp = 0;
 	var fb_posts = [];
 	var facebook_chart_data = {};
+	var fb_gender_data = [0,0,0];
 
 	var getFacebookPosts = function(uri){
 
@@ -496,7 +517,6 @@ DASHBOARD.facebook = function(access_token){
 				//call getFacebook Posts again
 				getFacebookPosts(new_uri);
 			}else{
-				console.log(fb_posts.length);
 				var monthly_posts = [0,0,0,0,0,0,0,0,0,0,0,0];
 				var monthly_friends = [0,0,0,0,0,0,0,0,0,0,0,0];
 				var current_month = (new Date()).getMonth();
@@ -516,9 +536,6 @@ DASHBOARD.facebook = function(access_token){
 
 				monthly_posts = _.first(monthly_posts, num_months).reverse();
 				monthly_friends = _.first(monthly_friends, num_months).reverse();
-				
-				console.log(monthly_posts);
-				console.log(monthly_friends);
 
 				var labels = [];
 				for(var i=0;i<num_months;i++){
@@ -529,7 +546,6 @@ DASHBOARD.facebook = function(access_token){
 				}
 
 				labels.reverse();
-				console.log(labels);
 
 				facebook_chart_data.labels = labels;
 				facebook_chart_data.datasets = [
@@ -560,8 +576,112 @@ DASHBOARD.facebook = function(access_token){
 		});
 	}
 
-	var getFacebookLikes = function(){
+	var getFacebookLikes = function(uri){
+		$.getJSON(uri)
+		.done(function(data){
+			var likes = data["data"];
+			$("#fb_likes").append(likes.length);
+		})
+		.fail(function( jqxhr, textStatus, error ){
+			var err = textStatus + ', ' + error;
+			console.log( "Request Failed: " + err);
+		});
+	}
 
+	var getFacebookFriends = function(uri){
+		$.getJSON(uri)
+		.done(function(data){
+			$("#fb_friends").append(data["data"][0]["friend_count"]);
+		})
+		.fail(function( jqxhr, textStatus, error ){
+			var err = textStatus + ', ' + error;
+			console.log( "Request Failed: " + err);
+		});
+	}
+
+	var getFacebookWallCount = function(uri){
+		$.getJSON(uri)
+		.done(function(data){
+			$("#fb_wallcount").append(data["data"][0]["wall_count"]);
+		})
+		.fail(function( jqxhr, textStatus, error ){
+			var err = textStatus + ', ' + error;
+			console.log( "Request Failed: " + err);
+		});
+	}
+
+	var getFacebookFriendsGender = function(uri){
+		$.getJSON(uri)
+		.done(function(data){
+			_.each(data["data"], function(datum){
+				if(datum["sex"] === "female")
+					fb_gender_data[0]++;
+				else if(datum["sex"] === "male")
+					fb_gender_data[1]++;
+				else
+					fb_gender_data[2]++;
+			});
+
+			drawFacebookGenderPie();
+		})
+		.fail(function( jqxhr, textStatus, error ){
+			var err = textStatus + ', ' + error;
+			console.log( "Request Failed: " + err);
+		});	
+	}
+
+	var drawFacebookGenderPie = function(){
+
+		var categories = ["Female", "Male", "Unknown"];
+		var data = [
+			{
+				value: fb_gender_data[0],
+				color: "#F7195C"
+			},
+			{
+				value: fb_gender_data[1],
+				color: "#7AC3F0"
+			},
+			{
+				value: fb_gender_data[2],
+				color: "#595959"
+			}
+		];
+
+		var chart_options = {
+			segmentShowStroke: true,
+			segmentStrokeWidth: 0.5
+		};
+
+		var canvas_parent = $("#fb_gender_pie");
+		var canvas_width = canvas_parent.innerWidth();
+		var canvas = 0;
+		if($("#canvas_fb_gender_pie").length){
+			console.log("canvas exists:" + $("#canvas_fb_gender_pie"));
+			canvas = $("#canvas_fb_gender_pie");
+			canvas.attr("width", canvas_width).attr("height", parseInt(canvas_width/1.6180));
+		}else{
+			console.log("creating new canvas");
+			canvas = $("<canvas/>",{"id": "canvas_fb_gender_pie"})
+				.attr({"width": canvas_width, "height": parseInt(canvas_width/1.6180)});
+			canvas_parent.append(canvas);
+		}
+		var myPie = new Chart(canvas.get(0).getContext("2d")).Pie(data, chart_options);
+
+		//draw legend
+		var legend_ul = $("#fb_gender_pie_legend");
+		_.each(data, function(element, index, list){
+			legend_ul.append(
+				$('<li>').append(
+					$('<div>')
+						.css("background-color",element["color"])
+						.css("width","20px")
+						.css("height","20px")
+						.css("float","left")
+						.css("margin-right","5px"),
+					categories[index])
+			);
+		});
 	}
 
 	var drawFacebookCanvas = function(){
@@ -593,6 +713,10 @@ DASHBOARD.facebook = function(access_token){
 			drawFacebookCanvas();
 		},
 
+		redrawFacebookGenderPie: function(){
+			drawFacebookGenderPie();
+		},
+
 		drawFacebookChart: function(arg_num_months){
 			// initial URI
 			var uri = "https://graph.facebook.com/me/posts?access_token=" + access_token + "&limit=100";
@@ -603,6 +727,27 @@ DASHBOARD.facebook = function(access_token){
 			until_timestamp = parseInt(d.getTime()/1000);
 
 			getFacebookPosts(uri);
+		},
+
+		getFacebookNumbers: function(){
+
+			//get facebook friends
+			//var uri = "https://graph.facebook.com/556205163/friends?limit=5000&access_token=" + access_token;
+			var uri = "https://graph.facebook.com/fql?q=" + encodeURIComponent("SELECT friend_count FROM user WHERE uid = me()") + "&access_token=" + access_token;
+			getFacebookFriends(uri);
+
+			//get facebook likes
+			uri = "https://graph.facebook.com/556205163/likes?limit=5000&offset=0&access_token=" + access_token;
+			getFacebookLikes(uri);
+
+			//get facebook wall posts
+			uri = "https://graph.facebook.com/fql?q=" + encodeURIComponent("SELECT wall_count FROM user where uid = me()") + "&access_token=" + access_token;
+			getFacebookWallCount(uri);
+
+			//get facebook gender ratio
+			uri = "https://graph.facebook.com/fql?q=" + encodeURIComponent("SELECT sex FROM user where uid IN (SELECT uid1 FROM friend WHERE uid2 = me())") + "&access_token=" + access_token;
+			getFacebookFriendsGender(uri);
+
 		}
 
 	}
